e4e02545b927165606d9ae5493ec9d68f53d2d67
Jorge Luis Sanchez <50006106+jorgeluissanchez@users.noreply.github.com>
2023-04-04 21:53:00 -0500

    actualizacion 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,27 @@
 
 import Menu from "@/components/molecules/menu";
 import ColoredProgressBar from "@/components/atoms/progress_bar";
+import { useEffect, useState } from "react";
+import SplashScreen from "@/components/molecules/splash_screen";
+
 
 import ArrowNav from "@/components/molecules/arrow_nav";
 
 const Home = () => {
+  const [splashState, setSplashState] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSplashState(false);
+    }, 3000);
+  }, []);
 
   return (
-    <><Menu type="search"  text="Busca tu/s carrera/s..."  /><ColoredProgressBar progress={1} />
-    <ArrowNav direction="left" rightUrl="/subjects" centerText="Selecciona tu/s carrera/s" /> 
+
+    <>{splashState ? <SplashScreen /> : <>
+    <Menu type="search"  text="Busca tu/s carrera/s..."  />
+    <Menu type="search"  text="Busca tu/s carrera/s..."  /><ColoredProgressBar progress={1} />
+    <ArrowNav direction="left" rightUrl="/subjects" centerText="Selecciona tu/s carrera/s" /></> }
     </>
   );
 };
