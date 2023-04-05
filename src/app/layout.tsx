f30277d7eb517cd6f1c3133186d0b79e4f17b308
Jorge Luis Sanchez <50006106+jorgeluissanchez@users.noreply.github.com>
2023-04-04 21:46:40 -0500

    actualizacion 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
-"use client";
 
 import './globals.css'
-import SplashScreen from "@/components/molecules/splash_screen";
-import { useState, useEffect } from "react";
 
 export const metadata = {
   title: 'Kronario',
@@ -14,20 +11,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const [mostrarSplash, setMostrarSplash] = useState(true);
-
-  useEffect(() => {
-    // Lógica para mostrar u ocultar el splash
-    setTimeout(() => {
-      setMostrarSplash(false);
-    }, 2000);
-  }, []);
-
   return (
     <html lang="en">
       <body className='font-roboto'>
-        {mostrarSplash && <SplashScreen /> }
-        {!mostrarSplash && children }
+        { children }
       </body>
     </html>
   )
